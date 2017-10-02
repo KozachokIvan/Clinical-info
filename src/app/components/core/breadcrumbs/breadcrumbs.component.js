40389f659766130aef3e1974c37367ae2e8cce98
Ivan Kozachok <kozachokivan1@gmail.com>
2017-10-02 07:48:37 +0300

    fix breadcrumbs

--- src/app/components/core/breadcrumbs/breadcrumbs.component.js
+++ src/app/components/core/breadcrumbs/breadcrumbs.component.js
@@ -1,12 +1,16 @@
 class BreadcrumbsController {
     constructor() {
 
+        this.username = this.username || "New"
     }
 }
 
 const BreadcrumbsComponent = {
     templateUrl: 'app/components/core/breadcrumbs/breadcrumbs.component.html',
-    controller: BreadcrumbsController
+    controller: BreadcrumbsController,
+    bindings: {
+        username: '='
+    }
 };
 
 export { BreadcrumbsComponent };
